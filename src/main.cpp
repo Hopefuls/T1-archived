d9a7ba3a9380e85400a247d0ce55ff54b1e71dbb
Aurel B <aurel@hopefuls.de>
2023-04-13 08:58:55 +0200

    update ligghts

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,25 @@
 #include <Arduino.h>
 
-void setup() {
-  pinMode(16, OUTPUT);
-  pinMode(17, OUTPUT);
-  pinMode(18, OUTPUT);
-  pinMode(19, OUTPUT);
+int HEADLIGHT_LEFT_PIN = 18;
+int HEADLIGHT_RIGHT_PIN = 19;
+int BRAKE_LIGHT_LEFT_PIN = 17;
+int BRAKE_LIGHT_RIGHT_PIN = 16;
 
+void setup() {
+  pinMode(HEADLIGHT_LEFT_PIN, OUTPUT);
+  pinMode(HEADLIGHT_RIGHT_PIN, OUTPUT);
+  pinMode(BRAKE_LIGHT_LEFT_PIN, OUTPUT);
+  pinMode(BRAKE_LIGHT_RIGHT_PIN, OUTPUT);
 }
 
 void loop() {
-  digitalWrite(16, HIGH);
-  digitalWrite(17, HIGH);
-  digitalWrite(18, HIGH);
-  digitalWrite(19, HIGH);
+  digitalWrite(HEADLIGHT_LEFT_PIN, HIGH);
+  digitalWrite(HEADLIGHT_RIGHT_PIN, HIGH);
+  digitalWrite(BRAKE_LIGHT_LEFT_PIN, HIGH);
+  digitalWrite(BRAKE_LIGHT_RIGHT_PIN, HIGH);
+  delay(1000);
+  digitalWrite(HEADLIGHT_LEFT_PIN, LOW);
+  delay(500);
+  digitalWrite(HEADLIGHT_RIGHT_PIN, LOW);
+  delay(500);
 }
